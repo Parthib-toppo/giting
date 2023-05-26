91a65f3a48a3c399536e197f23734a7ca217d74c
Parthib Toppo <toppoparthib@gmail.com>
2023-05-26 16:48:28 +0530

    initial commit

---
+++ test1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ll long long
+
+int main(){
+    cout<<"hello git"<<endl;    
+}
